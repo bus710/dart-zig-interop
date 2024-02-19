64f80b9e0a47b78e730c956523ce0a6add01af62
bus710 <bus710@gmail.com>
2024-02-18 21:28:35 -0800

    progress

--- src/02-primitives/primitives_library/primitives.zig
+++ src/02-primitives/primitives_library/primitives.zig
@@ -3,16 +3,18 @@ const std = @import("std");
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var allocator = gpa.allocator();
 
-pub fn main() void {
+pub fn main() !void {
     // check for leaks
     defer std.debug.assert(gpa.deinit() == .ok);
 
     std.debug.print("3 + 5 = {d}\n", .{sum(3, 5)});
 
-    const mult: *i64 = multiply(3, 5);
+    const mult: *i64 = multiply(3, 5) orelse {
+        std.debug.print("failed to allocate\n", .{});
+        return error.OutOfMemory;
+    };
+    defer free_pointer(mult);
     std.debug.print("3 * 5 = {d}\n", .{mult.*});
-
-    free_pointer(mult);
 
     var sub_num: i64 = 3;
     std.debug.print("3 - 5 = {d}\n", .{subtract(&sub_num, 5)});
@@ -24,16 +26,13 @@ export fn sum(a: i64, b: i64) i64 {
     return a + b;
 }
 
-export fn multiply(a: i64, b: i64) callconv(.C) *i64 {
-    var mult = allocator.alloc(i64, 1) catch undefined;
+export fn multiply(a: i64, b: i64) callconv(.C) ?*i64 {
+    var mult = allocator.alloc(i64, 1) catch return null;
     mult[0] = a * b;
     return &mult[0];
 }
 
 export fn free_pointer(ptr: *i64) void {
-    if (ptr == undefined) {
-        return;
-    }
     const p: *[1]i64 = ptr;
     allocator.free(p);
 }
